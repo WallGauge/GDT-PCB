fbfc8c6a3869e53e818f7792a8078ee53de1aef1
John Rucker <JHRucker@outlook.com>
2021-01-25 09:40:45 -0600

    .

--- testMe.js
+++ testMe.js
@@ -1,7 +1,9 @@
 const GaugeCmdTxClass = require('./gaugeCmdTxClass');
 const cfg = require('./testConfig.json');
 
-const gcTx = new GaugeCmdTxClass(cfg.irdLedPiin);
+
+//const gcTx = new GaugeCmdTxClass(cfg.irdLedPin);
+const gcTx = new GaugeCmdTxClass(22);
 
 let cmdToSend = gcTx.encode(1, 8, 100);
 
